01eb10c34834eb50aa9cc45e1554e3d20fc0f3fa
Eluc <ElucGeek@users.noreply.github.com>
2023-03-17 16:19:11 +0100

    Add loop to extract pages
    Instead of fixed code for 2 pages maximum, a loop will generate a table for each page and concatenate them.

--- pdf_importer/Importers.py
+++ pdf_importer/Importers.py
@@ -8,7 +8,7 @@ import camelot
 from dateutil.parser import parse
 from pandas import concat
 
-
+# import Cembra (Certo
 def extract_cembra(filename):
     entries = []
 
@@ -30,27 +30,22 @@ def extract_cembra(filename):
 
     return entries
 
-
+# import SwissCards (Cashback Cards) statement
 def extract_cashback(filename):
     entries = []
 
-    # noinspection PyUnresolvedReferences
-    table1 = camelot.read_pdf(
-        filename,
-        pages='1',
-        flavor='stream',
-        table_areas=['50,320,560,50'],
-        columns=['120,530']
-    )
-    table2 = camelot.read_pdf(
-        filename,
-        pages='2-end',
-        flavor='stream',
-        table_areas=['50,800,560,50'],
-        columns=['120,530']
-    )
+    tables = []
+    for page in range(1, camelot.read_pdf(filename, pages='1', flavor='stream').n):
+        page_tables = camelot.read_pdf(
+            filename,
+            pages=str(page),
+            flavor='stream',
+            table_areas=['50,800,560,50'],
+            columns=['120,530']
+        )
+        tables += page_tables
 
-    df = concat([table1[0].df, table2[0].df])
+    df = concat([table.df for table in tables])
 
     for index, row in df.iterrows():
         try:
